cbf3a332bd34dbda5a6b97996ddd4dd3ace1d734
Seth Samuel <sethsml@hotmail.com>
2025-05-06 20:41:17 +1200

    refactor Card and Home components; improve styling and structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,30 @@
-import Navbar from '../components/Navbar'
-import styles from './Home.module.css'  
-import Card from '../components/Card'
+import Navbar from "../components/Navbar";
+import styles from "./Home.module.css";
+import Card from "../components/Card";
 
 import logo from "../assets/images/marketing_association_logo.jpg";
 
 export default function Home() {
   return (
-    <div className={styles.container} >
-        <Navbar/>
-        test
-// ---------------------- BIG IMAGE --------------------- //
-// ---------------------- CARD AREA --------------------- //
+    <div className={styles.container}>
+      <Navbar />
+      {/*// ---------------------- BIG IMAGE ---------------------  */}
+      {/* // ---------------------- CARD AREA --------------------- // */}
 
-<Card image={logo} title="test" subtitle="hello world" ></Card>
-
+      <div className={styles.cardHolder}>
+        <Card
+          image={logo}
+          title="Card 1"
+          subtitle="hello world"></Card>
+        <Card
+          image={logo}
+          title="Card 2"
+          subtitle="world hello"></Card>
+        <Card
+          image={logo}
+          title="card 3"
+          subtitle="Morning sunshine"></Card>
+      </div>
     </div>
-  )
+  );
 }
