966a3fc05883481b2ec119455ac25652f274454c
Seth Samuel <sethsml@hotmail.com>
2025-05-06 19:28:19 +1200

    implement Card component with image, title, and subtitle; update Home to include Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import styles from "./Card.module.css";
 
-export default function Card() {
+export default function Card({ image, title, subtitle }) {
   return (
-    <>
-      <div>Card</div>;
-    </>
+    <div className={styles.container}>
+      <img
+        src={image}
+        alt=""
+      />
+      <div className={styles.bottomText}>
+        <p className={styles.title}  >{title}</p>
+        <p className={styles.subtitle}>{subtitle}</p>
+      </div>
+    </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
 import Navbar from '../components/Navbar'
 import styles from './Home.module.css'  
+import Card from '../components/Card'
+
+import logo from "../assets/images/marketing_association_logo.jpg";
 
 export default function Home() {
   return (
     <div className={styles.container} >
         <Navbar/>
         test
+// ---------------------- BIG IMAGE --------------------- //
+// ---------------------- CARD AREA --------------------- //
+
+<Card image={logo} title="test" subtitle="hello world" ></Card>
+
     </div>
   )
 }
